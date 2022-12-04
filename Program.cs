cde3443a3b8664275beee5aa05b0a298db8667e2
Mikael Nitell <mikael@glenneth.se>
2022-12-04 19:08:56 +0100

    first 2 days

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using AoC2022;
+
+Console.WriteLine(Day02A());
+Console.WriteLine(Day02B());
+
+static int Day01A()
+{
+    return File.ReadAllText("Day01.txt").Split("\r\n\r\n").Select(s => s.Split("\r\n")).Select(elf => elf.Select(r => int.Parse(r))).Select(elf => elf.Sum()).Max();
+}
+static int Day01B()
+{
+    return File.ReadAllText("Day01.txt").Split("\r\n\r\n").Select(s => s.Split("\r\n")).Select(elf => elf.Select(r => int.Parse(r))).Select(elf => elf.Sum()).OrderByDescending(sum => sum).Take(3).Sum();
+}
+
+static int Day02A()
+{
+    return File.ReadAllLines("Day02.txt").Sum(r => Utils.ScoreRockPaperScissors(r));
+}
+
+static int Day02B()
+{
+    return File.ReadAllLines("Day02.txt").Select(r=> Utils.SelectStrategy(r)).Sum(r => Utils.ScoreRockPaperScissors(r));
+}
+
+
+
+
+
---
+++ Utils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022
+{
+    internal class Utils
+    {
+
+        public static int ScoreRockPaperScissors(string s)
+        {
+            switch (s)
+            {
+                case "A X":
+                    return 1 + 3;
+                case "A Y":
+                    return 2 + 6;
+                case "A Z":
+                    return 3 + 0;
+
+                case "B X":
+                    return 1 + 0;
+                case "B Y":
+                    return 2 + 3;
+                case "B Z":
+                    return 3 + 6;
+
+                case "C X":
+                    return 1 + 6;
+                case "C Y":
+                    return 2 + 0;
+                case "C Z":
+                    return 3 + 3;
+
+                default:
+                    throw new ArgumentException(s);
+            }
+        }
+
+        internal static string SelectStrategy(string s)
+        {
+            switch (s)
+            {
+                case "A Z":
+                    return "A Y";
+                case "A Y":
+                    return "A X";
+                case "A X":
+                    return "A Z";
+
+                case "B Z":
+                    return "B Z";
+                case "B Y":
+                    return "B Y";
+                case "B X":
+                    return "B X";
+
+                case "C Z":
+                    return "C X";
+                case "C Y":
+                    return "C Z";
+                case "C X":
+                    return "C Y";
+
+                default:
+                    throw new ArgumentException(s);
+            }
+        }
+    }
+}
